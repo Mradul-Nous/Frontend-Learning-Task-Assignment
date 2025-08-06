58122a670b335e47933baa4a024aebd2bdb719bd
mradul-nous <mradul@nousinfo.com>
2025-08-06 12:57:05 +0530

    commiting the assignments of day1 and day2

--- assign_Day1.js
+++ assign_Day1.js
@@ -1,1 +1,15 @@
-console.log("hello")
+// Create variables to represent:
+// •	Name, age, fresher (true/false), preferred language
+// •	Log them with console.log and typeof
+// •	Reassign preferred language from "HTML" to "JavaScript" and log again
+// 🧠 Purpose: Understand variables, data types, reassignment, and typeof
+
+
+let Name="mradul"
+let age=22
+let fresher=true
+let preferred_language="HTML"
+console.log("name->"+Name,"\nage->"+age,"\nisfresher->"+fresher,"\npreferred language->"+preferred_language)
+console.log(typeof Name, typeof age, typeof fresher, typeof preferred_language) //checking the type of all literal
+preferred_language="javascript"//reassigne preferred language
+console.log("preferrend language->"+preferred_language)
---
+++ assign_day2.js
@@ -0,0 +1,22 @@
+// ✅ Micro Assignment: Grading System
+// Write a script that:
+// •	Takes marks (hardcoded or via prompt)
+// •	Validates if marks are between 0–100
+// •	Prints grade using if/else or switch
+// •	Use ternary to check if passed or failed
+
+
+//NOTE : please run this program in browser console 
+
+let marks=parseInt(prompt("Please enter your marks"))
+if(isNaN(marks)) alert("please enter marks in number only")
+else if(marks<0 || marks>100 ) {
+    alert("please enter the marks between 0-100")
+}else if
+(marks<34 ){ alert("Unfortunately, you failed the exam "); process.exit()}
+else{
+(marks>=34 && marks <=60)?   alert("you grade is D") :
+(marks >60 && marks <=80) ?  alert("your grade is C"):
+(marks>80 && marks<=90) ?    alert("your grade is B"): alert("your grade is A")
+
+}
