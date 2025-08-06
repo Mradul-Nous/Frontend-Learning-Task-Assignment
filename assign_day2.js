8a2c45d6a0ef8042302789e4bf50afe1b8e542f8
mradul-nous <mradul@nousinfo.com>
2025-08-06 12:58:48 +0530

    commiting day2 assignment

--- assign_day2.js
+++ assign_day2.js
@@ -13,7 +13,7 @@ if(isNaN(marks)) alert("please enter marks in number only")
 else if(marks<0 || marks>100 ) {
     alert("please enter the marks between 0-100")
 }else if
-(marks<34 ){ alert("Unfortunately, you failed the exam "); process.exit()}
+(marks<34 ){ alert("Unfortunately, you failed the exam "); }
 else{
 (marks>=34 && marks <=60)?   alert("you grade is D") :
 (marks >60 && marks <=80) ?  alert("your grade is C"):
